d0ee6f4485da0da614814cfd0ce621d827a75369
Yan Garcia <yan.vaz@br.lactalis.com>
2023-11-24 16:51:59 -0300

    feat: simple example of how to put a archive for download

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
 import Welcome from "./components/Welcome";
 
+import NOTFIS from './services/NOTFIS-mock.txt';
+
 export default function App() {
   return (
     <>
       <Welcome />
+      <a
+        href={NOTFIS}
+        download="Example-NOTFIS-Document"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <button>Baixar NOTFIS</button>
+      </a>
     </>
   );
 }
